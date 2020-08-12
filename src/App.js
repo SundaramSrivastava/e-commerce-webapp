37e375d2abd471fb89035e972c9b398fe44aeebf
SundaramSrivastava <srivastavasundaram4@gmail.com>
2020-08-12 23:50:06 +0530

    react-router first time use

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
+const Hats = () => {
+  return(
+    <div>
+      HATS
+    </div>
+  )
+}
+
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage}  /> 
+        <Route exact path='/shop/hats' component={Hats}  /> 
+      </Switch>
     </div>
   );
 }
